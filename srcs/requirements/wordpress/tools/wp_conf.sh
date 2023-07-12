4dc0a5edb6d9d8bbaded8ac1ae9bd2bfc7a50849
rertzer <rertzer@student.42.fr>
2023-07-12 14:55:35 +0200

    adminer in progress

--- srcs/requirements/wordpress/tools/wp_conf.sh
+++ srcs/requirements/wordpress/tools/wp_conf.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-echo "before sleep\n"
-#export PATH=$PATH:/usr/local/mysql/bin
-pwd
-sleep 10
-echo "after sleep\n"
+echo "having a nap\n"
+sleep 20
 
 if [ ! -f wp-config.php ]
 then
 	echo "Wordpress configuration\n"
-	#echo "step 1"
-	#wp core download --allow-root --path="/var/www/html/wordpress"
-
-	echo "step 2"
 	wp core config \
 		--dbhost=mariadb:3306 \
 		--dbname=$SQL_DATABASE \
@@ -26,12 +19,6 @@ then
 		--admin_email=$WP_EMAIL \
 		--url=$WP_URL \
 		--allow-root
-	#cd /wordpress
-	#sed -i "s/username_here/$SQL_USER/g" wp-config-sample.php
-	#sed -i "s/password_here/$SQL_PSSWD/g" wp-config-sample.php
-	#sed -i "s/localhost/mariadb/g" wp-config-sample.php
-	#sed -i "s/database_name_here/$SQL_DATABASE/g" wp-config-sample.php
-	#cp wp-config-sample.php wp-config.php	
 fi
 echo "starting php-fpm\n"
 exec /usr/sbin/php-fpm7.4 -F
